f21ea021d0694839116091674a4d256ea5ac81f0
Ikaro Amorim e Silva <ikaro.amorim@gmail.com>
2020-10-12 16:10:55 -0300

    :rocket: Map with openstreetMap

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,25 +1,13 @@
 import React from 'react';
+import "./styles/global.css"
 
-interface ITitleProps{
-  text:string
-}
+import Routes from './routes'
 
-function Title(props: ITitleProps){
-  return (
-  <h1>ahahaha {props.text}</h1>
-  )
-}
 
 function App() {
   return (
-    <div className="App">
-      <Title text="Título 1"/>
-      <Title text="Título 2"/>
-      <Title text="Título 3"/>
-      <Title text="Título 4"/>
-      <h1>Hello World</h1>
-    </div>
-  );
+    <Routes />
+  )
 }
 
 export default App;
