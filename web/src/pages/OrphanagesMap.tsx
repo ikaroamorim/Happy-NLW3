d6d6f5d68f875f2d61e256bf32eb681322729829
Ikaro Amorim e Silva <ikaro.amorim@gmail.com>
2020-10-12 16:23:05 -0300

    :rocket: End of day one

--- web/src/pages/OrphanagesMap.tsx
+++ web/src/pages/OrphanagesMap.tsx
@@ -27,7 +27,8 @@ function OrphanagesMap() {
       center={[-23.6106794,-46.6222736]}
       zoom = {15}
       style = {{width: '100%', height: '100%'}}>
-        <TileLayer url="https://a.tile.openstreetmap.org/{z}/{x}/{y}.png"/>
+        {/* <TileLayer url="https://a.tile.openstreetmap.org/{z}/{x}/{y}.png"/>*/}
+        <TileLayer url={`https://api.mapbox.com/styles/v1/mapbox/light-v10/tiles/256/{z}/{x}/{y}@2x?access_token=${process.env.REACT_APP_MAPBOX_TOKEN}`}/>
         </Map>
 
       <Link to="" className="create-orphanage">
