da9ccbe5ff67f23bb8d87e5212e797b4f3a02b6a
Ikaro Amorim e Silva <ikaro.amorim@gmail.com>
2020-10-12 11:40:21 -0300

    :rocket: Interface Example

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,9 +1,22 @@
 import React from 'react';
 
+interface ITitleProps{
+  text:string
+}
+
+function Title(props: ITitleProps){
+  return (
+  <h1>ahahaha {props.text}</h1>
+  )
+}
 
 function App() {
   return (
     <div className="App">
+      <Title text="Título 1"/>
+      <Title text="Título 2"/>
+      <Title text="Título 3"/>
+      <Title text="Título 4"/>
       <h1>Hello World</h1>
     </div>
   );
--- web/src/index.tsx
+++ web/src/index.tsx
@@ -2,8 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 
+
+
+//Strict mode is not used by Diego
 ReactDOM.render(
-  <React.StrictMode>
+  <React.StrictMode> 
     <App />
   </React.StrictMode>,
   document.getElementById('root')
