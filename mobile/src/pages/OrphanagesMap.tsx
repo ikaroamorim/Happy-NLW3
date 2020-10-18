69f744fbb58d00f8d708472532a65586a7ad449e
Ikaro Amorim e Silva <ikaro.amorim@gmail.com>
2020-10-17 22:35:53 -0300

    :rocket: Conclusão do Header Mobile Component

--- mobile/src/pages/OrphanagesMap.tsx
+++ mobile/src/pages/OrphanagesMap.tsx
@@ -1,16 +1,20 @@
 import React from 'react';
-import { StyleSheet, Text, View, Dimensions, TouchableOpacity } from 'react-native';
+import { StyleSheet, Text, View, Dimensions } from 'react-native';
 import MapView, { Marker, Callout, PROVIDER_GOOGLE } from 'react-native-maps';
 import { Feather} from '@expo/vector-icons';
 import { useNavigation } from '@react-navigation/native'
 
 import mapMarker from '../images/map-marker.png';
+import { RectButton } from 'react-native-gesture-handler';
 
 export default function OrphanagesMap() {
   const navigation = useNavigation();
 
+  function handleNavigateToCreateOrphanage(){
+    navigation.navigate('SelectMapPosition')
+  }
+
   function handleNavigateToOrphanageDetails(){
-    alert('navegou');
     navigation.navigate('OrphanageDetails');
     
   }
@@ -50,11 +54,11 @@ export default function OrphanagesMap() {
 
       <View style={styles.footer}>
         <Text style={styles.footerText}>Encontrado 2 orfanatos</Text>
-        <TouchableOpacity
+        <RectButton
           style={styles.createOrphanageButton}
-          onPress={() => { alert('Plus Button') }}>
+          onPress={() => { handleNavigateToCreateOrphanage }}>
             <Feather name="plus" size={20} color="#FFF" />
-        </TouchableOpacity>
+        </RectButton>
       </View>
     </View>
   )
--- mobile/src/routes.tsx
+++ mobile/src/routes.tsx
@@ -7,17 +7,48 @@ const { Navigator, Screen } = createStackNavigator();
 
 import OrphanagesMap from './pages/OrphanagesMap';
 import OrphanageDetails from './pages/OrphanageDetails';
+import OrphanageMapPosition from './pages/CreateOrphanages/SelectMapPosition';
+import OrphanageData from './pages/CreateOrphanages/OrphanageData';
+
+import Header from './components/Header'
+
 
 export default function Routes() {
   return (
     <NavigationContainer>
-      <Navigator screenOptions={{ headerShown: false }}>
+      <Navigator screenOptions={{ headerShown: false, cardStyle: {backgroundColor: '#f2f3f5'} }}>
         <Screen
           name="OrphanagesMap"
-          component={OrphanagesMap} />
-          <Screen
+          component={OrphanagesMap}
+        />
+
+        <Screen
           name="OrphanageDetails"
-          component={OrphanageDetails} />
+          component={OrphanageDetails}
+          options={{
+            headerShown:true,
+            header: () => <Header showCancel={false} title="Orfanato"/>
+          }}
+        />
+
+        <Screen
+          name="SelectMapPosition"
+          component={OrphanageMapPosition}
+          options={{
+            headerShown:true,
+            header: () => <Header title="Selecione no mapa"/>
+          }}
+        />
+
+        <Screen
+          name="OrphanageData"
+          component={OrphanageData}
+          options={{
+            headerShown:true,
+            header: () => <Header title="Informe os Dados"/>
+          }}
+        />
+
       </Navigator>
     </NavigationContainer>
   )
